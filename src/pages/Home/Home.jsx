fd38d3e8023d826fe44731214439edc41e6f3cd0
Dyonathan Cox <dyonathamcox@gmail.com>
2025-03-12 18:56:39 -0300

    correção

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
-import { Container, Button, Content, Section, Title  } from "./styles";
+import { Container, Button, Content, Section, Title } from "./styles";
 
-import { FiPlus  } from 'react-icons/fi';
+import { FiPlus } from 'react-icons/fi';
 
 import { Header } from "../../Components/Header";
 import { Note } from "../../Components/Note";
@@ -9,86 +9,86 @@ import { Note } from "../../Components/Note";
 export function Home() {
   return (
     <Container>
-      <Header />     
+      <Header />
 
       <Title>
-          <h1>Meus filmes</h1>
-            <Button to="/new" >
-              <FiPlus />
-              <span>Adiconar Filme</span>
-            </Button>  
-        </Title>
+        <h1>Meus filmes</h1>
+        <Button to="/new" >
+          <FiPlus />
+          <span>Adiconar Filme</span>
+        </Button>
+      </Title>
       <Content>
         <Section>
-            <Note data={{
-                    title: 'Interestellar',
-                    rating: 4,
-                    description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
-                    tags: [
-                      {id: '1', name:'Ficção Científica'},
-                      {id: '2', name:'Drama'},
-                      {id: '3', name:'Família'}
-                    ], 
-              }} />
+          <Note data={{
+            title: 'Interestellar',
+            rating: 4,
+            description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
+            tags: [
+              { id: '1', name: 'Ficção Científica' },
+              { id: '2', name: 'Drama' },
+              { id: '3', name: 'Família' }
+            ],
+          }} />
 
-            <Note data={{
-                    title: 'Interestellar',
-                    rating: 4,
-                    description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
-                    tags: [
-                      {id: '1', name:'Ficção Científica'},
-                      {id: '2', name:'Drama'},
-                      {id: '3', name:'Família'}
-                    ], 
-              }} />
+          <Note data={{
+            title: 'Interestellar',
+            rating: 4,
+            description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
+            tags: [
+              { id: '1', name: 'Ficção Científica' },
+              { id: '2', name: 'Drama' },
+              { id: '3', name: 'Família' }
+            ],
+          }} />
 
-<Note data={{
-                    title: 'Interestellar',
-                    rating: 4,
-                    description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
-                    tags: [
-                      {id: '1', name:'Ficção Científica'},
-                      {id: '2', name:'Drama'},
-                      {id: '3', name:'Família'}
-                    ], 
-              }} />
+          <Note data={{
+            title: 'Interestellar',
+            rating: 4,
+            description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
+            tags: [
+              { id: '1', name: 'Ficção Científica' },
+              { id: '2', name: 'Drama' },
+              { id: '3', name: 'Família' }
+            ],
+          }} />
 
-<Note data={{
-                    title: 'Interestellar',
-                    rating: 4,
-                    description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
-                    tags: [
-                      {id: '1', name:'Ficção Científica'},
-                      {id: '2', name:'Drama'},
-                      {id: '3', name:'Família'}
-                    ], 
-              }} />
+          <Note data={{
+            title: 'Interestellar',
+            rating: 4,
+            description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
+            tags: [
+              { id: '1', name: 'Ficção Científica' },
+              { id: '2', name: 'Drama' },
+              { id: '3', name: 'Família' }
+            ],
+          }} />
 
-<Note data={{
-                    title: 'Interestellar',
-                    rating: 4,
-                    description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
-                    tags: [
-                      {id: '1', name:'Ficção Científica'},
-                      {id: '2', name:'Drama'},
-                      {id: '3', name:'Família'}
-                    ], 
-              }} />
+          <Note data={{
+            title: 'Interestellar',
+            rating: 4,
+            description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
+            tags: [
+              { id: '1', name: 'Ficção Científica' },
+              { id: '2', name: 'Drama' },
+              { id: '3', name: 'Família' }
+            ],
+          }} />
 
-<Note data={{
-                    title: 'Interestellar',
-                    rating: 4,
-                    description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
-                    tags: [
-                      {id: '1', name:'Ficção Científica'},
-                      {id: '2', name:'Drama'},
-                      {id: '3', name:'Família'}
-                    ], 
-              }} />
+          <Note data={{
+            title: 'Interestellar',
+            rating: 4,
+            description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper',
+            tags: [
+              { id: '1', name: 'Ficção Científica' },
+              { id: '2', name: 'Drama' },
+              { id: '3', name: 'Família' }
+            ],
+          }} />
 
-                  
+
         </Section>
-        
+
       </Content>
 
     </Container>
